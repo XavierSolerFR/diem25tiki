8a92031f9ae05bc3ca015748bf224686e4fd7c51
Xavier Soler <xaviersoler@MBP13XSOLER.local>
2016-06-08 09:34:37 +0200

    mailman.local

---
+++ Services/mailman.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xaviersoler
+ * Date: 08/06/2016
+ * Time: 09:27
+ */
+$pearpath="/home3/solerx/php";
+$MailManSite="http://diem25fr.org/mailman/admin";
+$MailManPass="Platon+1993";
+$MailManListe = array("volontaires_diem25fr.org", "discussion_diem25fr.org");
--- Services/mailman_lib.php
+++ Services/mailman_lib.php
@@ -5,8 +5,10 @@
  * Date: 20/05/2016
  * Time: 23:14
  */
+include_once "Services/mailman.local.php";
+
 function MyMailMan($useremail, $site, $listes, $password){
-    ini_set("include_path", "/home3/solerx/php:" . ini_get("include_path") );
+    ini_set("include_path", "$pearpath:" . ini_get("include_path") );
     require_once 'Services/Mailman.php';
     $msg ="";
     foreach ($listes as $liste){
@@ -27,8 +29,5 @@ function MyMailMan($useremail, $site, $listes, $password){
 
 function AddToMailManList($email)
 {
-    $MailManSite="http://diem25fr.org/mailman/admin";
-    $mailmanpass="Platon+1993";
-    $liste = array("volontaires_diem25fr.org", "discussion_diem25fr.org");
-    return MyMailMan($email, $MailManSite, $liste, $mailmanpass);
+    return MyMailMan($email, $MailManSite, $MailManliste, $MailManPass);
 }
